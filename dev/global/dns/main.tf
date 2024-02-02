3f3bf38fae04d02a8f3d518f492c6a522c107fa9
ranshihc <158132849+ranshihc@users.noreply.github.com>
2024-02-02 14:40:47 +0800

    Update main.tf

--- dev/global/dns/main.tf
+++ dev/global/dns/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
   region     = "ap-northeast-1"
 }
 
-#data "aws_vpcs" "vpc_info" {
-#  tags = {
-#    env = "dev"
-#    bu  = "ran"
-#  }
-#}
+data "aws_vpcs" "vpc_info" {
+  tags = {
+    env = "dev"
+    bu  = "ran"
+  }
+}
 
 module "dns" {
   source  = "app.terraform.io/ranmax0215/dns/aws"
